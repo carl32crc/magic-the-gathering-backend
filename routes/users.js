a603000af6244a52e6f82c74334365a1bcdd4603
Carl Ruiz <carl.45.cr@gmail.com>
2017-09-15 00:10:12 +0200

    indentation controllers, middlewares in routes

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,13 @@ const multiparty = require('connect-multiparty')
 const mdUpload = multiparty({ uploadDir: './uploads/users' })
 
 // controllers
-const saveUser = require('../controllers/users/save_user')
-const login = require('../controllers/users/login')
+const saveUser    = require('../controllers/users/save_user')
+const login       = require('../controllers/users/login')
 const getAllUsers = require('../controllers/users/get_all_users')
-const deleteUser = require('../controllers/users/delete_user')
-const updateUser = require('../controllers/users/update_user')
-const uploadImg = require('../controllers/image/upload_image')
+const deleteUser  = require('../controllers/users/delete_user')
+const updateUser  = require('../controllers/users/update_user')
+
+const uploadImg    = require('../controllers/image/upload_image')
 const getImageFile = require('../controllers/image/get_image')
 
 // models
@@ -20,7 +21,7 @@ const User = require('../models/users')
 
 // middlewares
 const ensureAuth = require('../middlewares/authenticated')
-const isAdmin = require('../middlewares/is_admin')
+const isAdmin    = require('../middlewares/is_admin')
 
 api.post('/register', saveUser )
 api.post('/login', login )
